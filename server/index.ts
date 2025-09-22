85f5e0e3ce70066daaf2b47cb6d3bf70b4dd7a11
suniljas <jassticky@gmail.com>
2025-09-22 17:02:19 +0530

    Working In Local Code

--- server/index.ts
+++ server/index.ts
@@ -81,12 +81,9 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client
   const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-    log(`WebSocket server ready at ws://localhost:${port}/ws`);
-  });
+  server.listen(port, "127.0.0.1", () => {
+  log(`serving on port ${port}`);
+  log(`WebSocket server ready at ws://localhost:${port}/ws`);
+});
+
 })();
